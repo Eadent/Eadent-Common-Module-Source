2a01910cbc1aeaa004954b3dd6bc3f3d6d4478aa
Eamonn Duffy <Eamonn@Duffy.global>
2022-05-05 17:30:48 +0100

    Added Register User Data Transfer Objects.

---
+++ WebApi/DataTransferObjects/Sessions/Users/RegisterUserRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Eadent.Common.WebApi.DataTransferObjects.Sessions.Users
+{
+    public class RegisterUserRequestDto
+    {
+        [JsonPropertyName("createdByApplicationId")]
+        int CreatedByApplicationId { get; set; }
+
+        [JsonPropertyName("userGuidString")]
+        public string UserGuidString { get; set; }
+
+        [JsonPropertyName("roleId")]
+        short RoleId { get; set; }
+
+        [JsonPropertyName("displayName")]
+        string DisplayName { get; set; }
+
+        [JsonPropertyName("eMailAddress")]
+        public string EMailAddress { get; set; }
+
+        [JsonPropertyName("mobilePhoneNumber")]
+        public string MobilePhoneNumber { get; set; }
+
+        [JsonPropertyName("plainTextPassword")]
+        public string PlainTextPassword { get; set; }
+    }
+}
---
+++ WebApi/DataTransferObjects/Sessions/Users/RegisterUserResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+namespace Eadent.Common.WebApi.DataTransferObjects.Sessions.Users
+{
+    public class RegisterUserResponseDto
+    {
+        [JsonPropertyName("registerUserStatusId")]
+        short RegisterUserStatusId { get; set; }
+
+        [JsonPropertyName("userId")]
+        public long UserId { get; set; }
+
+        [JsonPropertyName("userGuidString")]
+        public string UserGuidString { get; set; }
+    }
+}
