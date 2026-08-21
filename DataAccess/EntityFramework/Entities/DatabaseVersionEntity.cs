8b9796f05e5185acbe6972c5078a0c5713bfcb5c
Eamonn Duffy <Eamonn.Duffy@GMail.com>
2026-08-21 22:02:32 +0100

    feat: Preliminary Database Version support.

---
+++ DataAccess/EntityFramework/Entities/DatabaseVersionEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eadent.Common.DataAccess.EntityFramework.Entities
+{
+    public class DatabaseVersionEntity
+    {
+        [Key]
+        public int DatabaseVersionId { get; set; }
+
+        public int Major { get; set; }
+
+        public int Minor { get; set; }
+
+        public int Patch { get; set; }
+
+        public string Build { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDateTimeUtc { get; set; }
+
+        public DateTime? LastUpdatedDateTimeUtc { get; set; }
+    }
+}
