c87548db1e9b8435e71d80c7a3aede40f699704a
Eamonn Duffy <Eamonn.Duffy@GMail.com>
2026-08-21 23:11:38 +0100

    feat: Added Database Version Enquiry Support.

---
+++ DataAccess/EntityFramework/Repositories/IDatabaseVersionsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Eadent.Common.DataAccess.EntityFramework.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eadent.Common.DataAccess.EntityFramework.Repositories
+{
+    public interface IDatabaseVersionsRepository : IBaseRepository<DatabaseVersionEntity, int>
+    {
+        Task<DatabaseVersionEntity> GetLatestOrDefaultAsync(CancellationToken cancellationToken = default);
+    }
+}
