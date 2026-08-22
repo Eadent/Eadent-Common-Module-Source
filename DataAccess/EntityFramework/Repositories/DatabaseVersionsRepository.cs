ac5892968035d1e305399d5e6fe7b49b7e20caf6
Eamonn Duffy <Eamonn.Duffy@GMail.com>
2026-08-22 07:31:38 +0100

    feat: Added DatabaseVersionsRepository base class to reduce code duplication.

---
+++ DataAccess/EntityFramework/Repositories/DatabaseVersionsRepository.cs
@@ -0,0 +1,24 @@
+﻿using Eadent.Common.DataAccess.EntityFramework.Databases;
+using Eadent.Common.DataAccess.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eadent.Common.DataAccess.EntityFramework.Repositories
+{
+    public class DatabaseVersionsRepository<TDatabase> : BaseRepository<TDatabase, DatabaseVersionEntity, int>, IDatabaseVersionsRepository
+        where TDatabase : IBaseDatabase
+    {
+        public DatabaseVersionsRepository(TDatabase database) : base(database)
+        {
+        }
+
+        public async Task<DatabaseVersionEntity> GetLatestOrDefaultAsync(CancellationToken cancellationToken = default)
+        {
+            return await Database.Context.Set<DatabaseVersionEntity>()
+                .OrderByDescending(entity => entity.DatabaseVersionId)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
